dd492073fce8569d5676c45ac4495cc898f87462
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-01-02 23:34:18 +0000

    update

--- Models/PermissionCategory.php
+++ Models/PermissionCategory.php
@@ -17,7 +17,7 @@ namespace Modules\Search\Models;
 use phpOMS\Stdlib\Base\Enum;
 
 /**
- * Permision state enum.
+ * Permission category enum.
  *
  * @package Modules\Search\Models
  * @license OMS License 2.0
